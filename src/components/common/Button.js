2e08ce707f5aa2c21c090caaf740a5aa5b97c55f
Suyeon <msjgr2801@gmail.com>
2023-05-09 14:36:39 +0900

    Refactoring: Slide Button 리팩토링

--- src/components/common/Button.js
+++ src/components/common/Button.js
@@ -1,26 +1,10 @@
 import React from 'react'
 import "./Button.css";
 
-const sizes = ['long-button', 'short-button', 'search-button'];
-
 const Button = ({buttonSize, content, type, disable}) => {
-  const buttonSelect = () => {
-    if (buttonSize === sizes[0]) {
-      return 'long_button';
-    }
-
-    else if (buttonSize === sizes[1]) {
-      return 'short_button';
-    }
-
-    else if (buttonSize === sizes[2]) {
-      return 'search_button';
-    }
-  }
-
   return (
     <button
-      className={buttonSelect()}
+      className={buttonSize}
       type={type}
       disabled={disable === undefined ? false : disable}
     >
--- src/components/main/Slide.js
+++ src/components/main/Slide.js
@@ -2,7 +2,8 @@ import React, { useState, useEffect, useRef } from 'react'
 import { Link } from 'react-router-dom';
 
 import "./Slide.css";
-import { Card1Text, Card2Text, Card3Text } from './CardText';
+import { slideInfo } from 'constant/main';
+import SlideCard from './SlideCard';
 import Button from 'components/common/Button'
 import Pagination from './Pagination';
 
@@ -12,62 +13,45 @@ const IMAGE_LENGTH = 3;
 const Slide = () => {
   const [page, setPage] = useState(0);
   const [customInterval, setCustomInterval] = useState(4000);
-  const slideInfo = [
-    {
-      status: 0,
-      link: '/train',
-      content: '환승 시간표 확인하기'
-    }, 
-    {
-      status: 1,
-      link: '/map',
-      content: '길찾기 바로가기'
-    },
-    {
-      status: 2,
-      link: 'bus/:date',
-      content: '버스 시간표 확인하기'
-    }
-  ];
 
-  // useEffect(() => {
-  //   calStyle(page);
-  // }, [page]);
+  useEffect(() => {
+    calStyle(page);
+  }, [page]);
 
-  // useEffect(() => {
-  //   setCustomInterval(4000);
-  // }, [customInterval]);
+  useEffect(() => {
+    setCustomInterval(4000);
+  }, [customInterval]);
 
-  // useInterval(() => {
-  //   setPage((curr) => {
-  //     curr += 1;
-  //     if (curr >= IMAGE_LENGTH) {
-  //       curr = 0;
-  //     }
+  useInterval(() => {
+    setPage((curr) => {
+      curr += 1;
+      if (curr >= IMAGE_LENGTH) {
+        curr = 0;
+      }
       
-  //     return curr;
-  //   })
-  // }, customInterval);
+      return curr;
+    })
+  }, customInterval);
 
-  // function useInterval(callback, delay) {
-  //   const savedCallback = useRef();
+  function useInterval(callback, delay) {
+    const savedCallback = useRef();
   
-  //   // Remember the latest callback.
-  //   useEffect(() => {
-  //     savedCallback.current = callback;
-  //   }, [callback]);
+    // Remember the latest callback.
+    useEffect(() => {
+      savedCallback.current = callback;
+    }, [callback]);
   
-  //   // Set up the interval.
-  //   useEffect(() => {
-  //     function tick() {
-  //       savedCallback.current();
-  //     }
-  //     if (delay !== null) {
-  //       let id = setInterval(tick, delay);
-  //       return () => clearInterval(id);
-  //     }
-  //   }, [delay]);
-  // }
+    // Set up the interval.
+    useEffect(() => {
+      function tick() {
+        savedCallback.current();
+      }
+      if (delay !== null) {
+        let id = setInterval(tick, delay);
+        return () => clearInterval(id);
+      }
+    }, [delay]);
+  }
 
   console.log(page);
 
@@ -75,10 +59,10 @@ const Slide = () => {
     setCustomInterval(500);
   }
 
-  function calStyle (curr) {
+  function calStyle (page) {
     const slideWidth = document.querySelector('.slide').clientWidth;
     const ul = document.querySelector('ul');
-    switch(curr) {
+    switch(page) {
       case 0:
         ul.setAttribute('style', `left: ${0}px`)
         break;
@@ -94,24 +78,13 @@ const Slide = () => {
     }
   }
 
-  function Card({status}) {
-    return (
-      <>
-        <div className="slide-img"></div>
-        {status === 0 ? <Card1Text /> : ''}
-        {status === 1 ? <Card2Text /> : ''}
-        {status === 2 ? <Card3Text /> : ''}
-      </>
-    )
-  }
-
-  function SlideItem({status, link, content}) {
+  function SlideList({status, link, content}) {
     return (
       <li>
-        <Card status={status}/>
-        <div className="slide-button">
+        <SlideCard status={status}/>
+        <div className="container-button">
           <Link to={link}>
-            <Button buttonsSize={'long-button'} content={content}/>
+            <Button buttonSize={'button-slide'} content={content}/>
           </Link>
         </div>
       </li>
@@ -122,7 +95,7 @@ const Slide = () => {
     <div className="slide">
       <ul>
         {slideInfo.map((e, i) => {
-          return <SlideItem key={i} status={e.status} link={e.link} content={e.content}/>
+          return <SlideList key={i} status={e.status} link={e.link} content={e.content}/>
         })}
       </ul>
       <Pagination n={3} page={page} setPage={setPage} delay={delayPagination}/>
