6427f7dd00e68af0daa00e37afda8ece9495d12a
hyunah9 <hyn2626@naver.com>
2023-05-12 23:59:29 +0900

    delete code

--- src/components/busTime/HeaderBus.js
+++ src/components/busTime/HeaderBus.js
@@ -6,9 +6,9 @@ const HeaderBus = (props) => {
   return (
     <>
     <div className="bus-date">
-      <Link onClick={()=>{props.setDate("weekday") ; }} className={`bus-date-space ${props.date ==="weekday"? "ff" : "" }`} to={`/bus/weekday`} style={{ textDecoration: 'none' , color:"white"}}>평일</Link>
-      <Link onClick={()=>{props.setDate("campusHoliday"); }} className={`bus-date-space ${props.date ==="campusHoliday"? "ff" : "" }`}  to='../bus/campusHoliday' style={{ textDecoration: 'none' , color:"white"}}>대학만 방학</Link>  
-      <Link onClick={()=>{props.setDate("holiday"); }} className={`bus-date-space ${props.date ==="holiday"? "ff" : "" }`}  to='../bus/holiday' style={{ textDecoration: 'none' , color:"white"}}>모두 방학</Link>
+      <Link onClick={()=>{props.setDate("weekday") ; }} className={`bus-date-space ${props.date ==="weekday"? "select-date" : "" }`} to={`/bus/weekday`} style={{ textDecoration: 'none' , color:"white"}}>평일</Link>
+      <Link onClick={()=>{props.setDate("campusHoliday"); }} className={`bus-date-space ${props.date ==="campusHoliday"? "select-date" : "" }`}  to='../bus/campusHoliday' style={{ textDecoration: 'none' , color:"white"}}>대학만 방학</Link>  
+      <Link onClick={()=>{props.setDate("holiday"); }} className={`bus-date-space ${props.date ==="holiday"? "select-date" : "" }`}  to='../bus/holiday' style={{ textDecoration: 'none' , color:"white"}}>모두 방학</Link>
     </div>
     </>
 
