babf8663d47503e7000f4dce76428744570a2d3b
Suyeon <msjgr2801@gmail.com>
2023-08-31 12:11:42 +0900

    Feat: RoadFindPage PC 반응형 스타일 제작

--- src/pages/RoadFindPage.js
+++ src/pages/RoadFindPage.js
@@ -2,15 +2,24 @@ import 'styles/road-find-page/road-find-page.css';
 import Map from 'components/RoadFind/Map';
 import DestinationInfo from 'components/RoadFind/DestinationInfo';
 import { useState } from 'react';
+import Loading from 'components/Common/Loading';
 
 function RoadFindPage() {
-  const [userLocation, setUserLocation] = useState('부산대학교');
-  return (
-    <div className='road-find-page'>
-      <Map userLocation={userLocation} setUserLocation={setUserLocation} />
-      <DestinationInfo startPoint={'부산대학교'} destinationPoint={userLocation} />
-    </div>
-  )
+	const [loading, setLoading] = useState(false);
+	const [userLocation, setUserLocation] = useState('부산대학교');
+	return (
+		<div className='road-find-page'>
+			{loading && <Loading />}
+			<Map userLocation={userLocation} setUserLocation={setUserLocation} />
+			<div className='destination-info'>
+				<DestinationInfo
+					startPoint={'부산대학교'}
+					destinationPoint={userLocation}
+					setLoading={setLoading}
+				/>
+			</div>
+		</div>
+	);
 }
 
 export default RoadFindPage;
