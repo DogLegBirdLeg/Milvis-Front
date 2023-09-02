4a0ee6007a68612bf61956cf5eb15cc66f9726c5
Suyeon <msjgr2801@gmail.com>
2023-09-02 16:09:01 +0900

    Refacotring: Map 컴포넌트 재사용을 위해 이벤트 부여 부분 분리하기

--- src/components/RoadFind/Map.js
+++ src/components/RoadFind/Map.js
@@ -1,13 +1,10 @@
 import { useEffect, useRef } from 'react';
-import { changeLocation } from 'utils/RoadFind/changeLocation';
 import { MAP_OPTIONS } from 'utils/Constant';
 
 /*global kakao*/
-function Map({ userLocation, setUserLocation }) {
+function Map({ setMap }) {
 	const mapRef = useRef();
 	const { MAP_INIT_LAT, MAP_INIT_LNG, MAP_LEVEL } = MAP_OPTIONS;
-
-	useEffect(() => {}, []);
 
 	useEffect(() => {
 		const mapOption = {
@@ -15,11 +12,9 @@ function Map({ userLocation, setUserLocation }) {
 			level: MAP_LEVEL,
 		};
 		const map = new kakao.maps.Map(mapRef.current, mapOption);
+		setMap(map);
 
-		kakao.maps.event.addListener(map, 'idle', () => {
-      changeLocation(map, setUserLocation)
-		});
-	}, [MAP_INIT_LAT, MAP_INIT_LNG, MAP_LEVEL, setUserLocation]);
+	}, [MAP_INIT_LAT, MAP_INIT_LNG, MAP_LEVEL, setMap]);
 
 	return <div ref={mapRef} id='map' className='map'></div>;
 }
--- src/pages/RoadFindPage.js
+++ src/pages/RoadFindPage.js
@@ -1,16 +1,28 @@
 import 'styles/road-find-page/road-find-page.css';
 import Map from 'components/RoadFind/Map';
 import DestinationInfo from 'components/RoadFind/DestinationInfo';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import Loading from 'components/Common/Loading';
+import { changeLocation } from 'utils/RoadFind/changeLocation';
 
+/*global kakao*/
 function RoadFindPage() {
 	const [loading, setLoading] = useState(false);
 	const [userLocation, setUserLocation] = useState('부산대학교');
+	const [map, setMap] = useState(undefined);
+
+	useEffect(() => {
+		if (map) {
+			kakao.maps.event.addListener(map, 'idle', () => {
+				changeLocation(map, setUserLocation)
+			});
+		}
+	}, [map]);
+
 	return (
 		<div className='road-find-page'>
 			{loading && <Loading />}
-			<Map userLocation={userLocation} setUserLocation={setUserLocation} />
+			<Map setMap={setMap} />
 			<div className='destination-info'>
 				<DestinationInfo
 					startPoint={'부산대학교'}
