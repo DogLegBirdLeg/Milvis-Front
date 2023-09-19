516b09ac193370fd4ad4496f5bf5a874878bc2f8
Suyeon <msjgr2801@gmail.com>
2023-09-19 18:42:35 +0900

    Feat: RoadFinResultPage 구조 설계

--- src/pages/RoadFindResultPage.js
+++ src/pages/RoadFindResultPage.js
@@ -1,12 +1,33 @@
-import React from 'react'
+import { useEffect, useRef, useState } from 'react';
+import Map from 'components/RoadFind/Map';
 
+const RoadFindResult = () => {
+	const [stationData, setStationData] = useState([]);
+	const [selectedStation, setSelectedStation] = useState();
+	const mapRef = useRef(null);
 
-const BusResult = () => {
+	const getStationData = async () => {
+		const response = await fetch('');
 
-  return (
-    <main className='map-main'>
+		if (response.ok) {
+			return response.json();
+		}
+	};
 
-    </main>
-  )
-}
-export default BusResult;
+	const handleButtonClick = () => {
+		selectedStation(null);
+	};
+
+	useEffect(() => {
+		getStationData().then((data) => setStationData(data));
+	}, []);
+	return (
+		<main className='page'>
+			<Map ref={mapRef} />
+			{!selectedStation && stationData.length > 0 && <BusStationMarker />}
+			{selectedStation && <RoadInfo stationInfo={selectedStation} />}
+			<button onClick={handleButtonClick}>이전</button>
+		</main>
+	);
+};
+export default RoadFindResult;
