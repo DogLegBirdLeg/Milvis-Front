6e09064a1690397f734b5edbb778b99f77255ebb
Suyeon <msjgr2801@gmail.com>
2023-09-30 17:05:24 +0900

    Feat: 로딩, 에러 처리 시 로직 구현

---
+++ src/components/RoadFindResult/RoadInfo.js
@@ -0,0 +1,3 @@
+const RoadInfo = () => {};
+
+export default RoadInfo;
--- src/pages/RoadFindResultPage.js
+++ src/pages/RoadFindResultPage.js
@@ -1,29 +1,37 @@
 import { useEffect, useRef, useState } from 'react';
+
+import { getRoadResultData } from 'API/getRoadResultData';
+import { ALERT_MESSAGE } from 'utils/Constant';
 import Map from 'components/RoadFind/Map';
+import BusStationMarker from 'components/RoadFindResult/BusStationMarker';
+import RoadInfo from 'components/RoadFindResult/RoadInfo';
+import Loading from 'components/Common/Loading';
+import Alert from 'components/RoadFind/Alert';
 
 const RoadFindResult = () => {
+	const { NO_FIND_STATION, ERROR_MESSAGE } = ALERT_MESSAGE;
+	const [loading, setLoading] = useState(true);
+	const [error, setError] = useState(false);
 	const [stationData, setStationData] = useState([]);
 	const [selectedStation, setSelectedStation] = useState();
-	const mapRef = useRef(null);
-
-	const getStationData = async () => {
-		const response = await fetch('');
-
-		if (response.ok) {
-			return response.json();
-		}
-	};
+	const [map, setMap] = useState();
 
 	const handleButtonClick = () => {
 		selectedStation(null);
 	};
 
 	useEffect(() => {
-		getStationData().then((data) => setStationData(data));
+		getRoadResultData()
+			.then((data) => setStationData(data))
+			.catch(() => setError(true))
+			.finally(() => setLoading(false));
 	}, []);
+
 	return (
 		<main className='page'>
-			<Map ref={mapRef} />
+			<Map setMap={setMap} />
+			{loading && <Loading />}
+			{error && <Alert flag={ERROR_MESSAGE} />}
 			{!selectedStation && stationData.length > 0 && <BusStationMarker />}
 			{selectedStation && <RoadInfo stationInfo={selectedStation} />}
 			<button onClick={handleButtonClick}>이전</button>
