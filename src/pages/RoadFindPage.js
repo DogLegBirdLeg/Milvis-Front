cfdf4a34c219d51a837c0b8c1f5e55a30d628622
Suyeon <msjgr2801@gmail.com>
2023-09-30 16:45:44 +0900

    Feat: 위도, 경도 정보 따로 저장
    - 주소 정보와 위도, 경도 정보르 따로 관리
    - navigate 로 결과 페이지 이동시 위도, 경도 정보와 함께 전달

--- src/pages/RoadFindPage.js
+++ src/pages/RoadFindPage.js
@@ -2,42 +2,60 @@ import { useCallback, useEffect, useMemo, useState } from 'react';
 
 import 'styles/road-find-page/road-find-page.css';
 import { changeLocation } from 'utils/RoadFind/changeLocation';
-import { ALERT_MESSAGE } from 'utils/Constant';
+import { ALERT_MESSAGE, MAP_OPTIONS } from 'utils/Constant';
 import Map from 'components/RoadFind/Map';
 import Loading from 'components/Common/Loading';
 import Marker from 'components/RoadFind/Marker';
 import Alert from 'components/RoadFind/Alert';
 import DestinationInfo from 'components/RoadFind/DestinationInfo';
 import CenterPoint from 'components/RoadFind/CenterPoint';
+import { useNavigate } from 'react-router';
 
 /*global kakao*/
 function RoadFindPage() {
+	const navigate = useNavigate();
 	const eventTypes = useMemo(() => ['center_changed', 'click'], []);
 	const [loading, setLoading] = useState(false);
+	const [userLatLng, setUserLatLng] = useState({
+		x: MAP_OPTIONS.MAP_INIT_LAT,
+		y: MAP_OPTIONS.MAP_INIT_LNG,
+	});
 	const [userLocation, setUserLocation] = useState('부산대학교');
 	const [map, setMap] = useState(undefined);
 
-	const handleMarkerEvent = useCallback((marker, position) => {
-		if (!map) return;
+	const handleMarkerEvent = useCallback(
+		(marker, position) => {
+			if (!map) return;
 
-		marker.setPosition(position);
-		marker.setZIndex(3);
-	}, [map]);
+			marker.setPosition(position);
+			marker.setZIndex(3);
+		},
+		[map]
+	);
 
-	const handleRedrawEvent = useCallback((circle, position) => {
-		if (!map) return;
-		
-		circle.setPosition(position);
-	}, [map]);
+	const handleRedrawEvent = useCallback(
+		(circle, position) => {
+			if (!map) return;
+
+			circle.setPosition(position);
+		},
+		[map]
+	);
+
+	const moveResultPage = () => {
+		navigate('/road-find-result', { state: userLatLng });
+	};
 
 	useEffect(() => {
 		if (!map) return;
 		eventTypes.forEach((event) => {
 			kakao.maps.event.addListener(map, event, (mouseEvent) => {
 				const position = mouseEvent ? mouseEvent.latLng : map.getCenter();
+				console.log(position);
+				setUserLatLng(position);
 				changeLocation(position, setUserLocation);
 			});
-		})
+		});
 	}, [map, eventTypes]);
 
 	return (
@@ -51,10 +69,19 @@ function RoadFindPage() {
 				/>
 			</div>
 			<div className='road-find-page__map'>
-				<Marker map={map} eventType={eventTypes} handleMarkerEvent={handleMarkerEvent} />
+				<Marker
+					map={map}
+					eventType={eventTypes}
+					handleMarkerEvent={handleMarkerEvent}
+				/>
 				<Alert flag={ALERT_MESSAGE.SELECT_PLACE} />
-				<CenterPoint map={map} eventType={eventTypes} handleRedrawEvent={handleRedrawEvent} />
+				<CenterPoint
+					map={map}
+					eventType={eventTypes}
+					handleRedrawEvent={handleRedrawEvent}
+				/>
 				<Map setMap={setMap} />
+				<button onClick={moveResultPage}>다음</button>
 			</div>
 		</div>
 	);
