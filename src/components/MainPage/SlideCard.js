10a803541b47260a0d5ece9f2145a0a0ee83994e
Suyeon <msjgr2801@gmail.com>
2023-10-11 17:25:08 +0900

    Fix: 메인 화면 UI 수정

--- src/components/MainPage/SlideCard.js
+++ src/components/MainPage/SlideCard.js
@@ -1,45 +1,71 @@
-import React from 'react'
+import transferImg from 'assets/train_chien_woman.png';
+import roadFindImg from 'assets/map_open.png';
+import busTimeImg from 'assets/smartphone_map_app_man.png';
 
-function SlideCard({status}) {
-  function Card1Text() {
-    return (
-      <div className="slide-info-el">
-        <p>기차, 버스</p>
-        <p className='point'>시간표 비교</p>
-        <p>번거롭지 않으세요?</p>
-        <p>열차와 버스 시간표를 한 번에 조회해보세요.</p>
-      </div>
-    )
-  }
+function SlideCard({ status }) {
+	function Card1Text() {
+		return (
+			<div className='main-page__slide-info'>
+				<div className='main-page__slide-info-img-container'>
+					<img src={transferImg} alt='밀양 시간표 비교 이미지' />
+				</div>
+				<div className='main-page__slide-info-text'>
+					<p>기차, 버스</p>
+					<p className='point'>시간표 비교</p>
+					<p>번거롭지 않으세요?</p>
+					<p>열차와 버스 시간표를 한 번에 조회해보세요.</p>
+				</div>
+			</div>
+		);
+	}
 
-  function Card2Text() {
-    return (
-      <div className="slide-info-el">
-        <p className='point'>밀양시내,</p>
-        <p>알려드릴게요</p>
-        <p>목적지에서<br />가장 가까운 정류장을 알려드려요.</p>
-      </div>
-    )
-  }
+	function Card2Text() {
+		return (
+			<div className='main-page__slide-info'>
+				<div className='main-page__slide-info-img-container'>
+					<img src={roadFindImg} alt='밀양 길찾기 이미지' />
+				</div>
+				<div className='main-page__slide-info-text'>
+					<p className='point'>밀양시내,</p>
+					<p>알려드릴게요</p>
+					<p>
+						목적지에서
+						<br />
+						가장 가까운 정류장을 알려드려요.
+					</p>
+				</div>
+			</div>
+		);
+	}
 
-  function Card3Text() {
-    return (
-      <div className="slide-info-el">
-        <p>기존 <span className='point'>버스 시간표</span></p>
-        <p>를 찾고 계신가요?</p>
-        <p>더 직관적인<br/>시간표를 통해 한 눈에 확인해보세요.</p>
-      </div>
-    )
-  }
+	function Card3Text() {
+		return (
+			<div className='main-page__slide-info'>
+				<div className='main-page__slide-info-img-container'>
+					<img src={busTimeImg} alt='밀양 버스 시간표 확인 이미지' />
+				</div>
+				<div className='main-page__slide-info-text'>
+					<p>
+						기존 <span className='point'>버스 시간표</span>
+					</p>
+					<p>를 찾고 계신가요?</p>
+					<p>
+						더 직관적인
+						<br />
+						시간표를 통해 한 눈에 확인해보세요.
+					</p>
+				</div>
+			</div>
+		);
+	}
 
-  return (
-    <>
-      <div className="slide-img"></div>
-      {status === 0 ? <Card1Text /> : ''}
-      {status === 1 ? <Card2Text /> : ''}
-      {status === 2 ? <Card3Text /> : ''}
-    </>
-  )
+	return (
+		<>
+			{status === 0 ? <Card1Text /> : ''}
+			{status === 1 ? <Card2Text /> : ''}
+			{status === 2 ? <Card3Text /> : ''}
+		</>
+	);
 }
 
 export default SlideCard;
--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -1,17 +1,43 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
-import Slide from 'components/MainPage/Slide'
-import "styles/main-page/main-page.css";
+import 'styles/main-page/main-page.css';
+import Button from 'components/Common/Button';
+import { SLIDE_INFO } from 'utils/Constant';
+import SlideCard from 'components/MainPage/SlideCard';
 
 const MainPage = () => {
-  return (
-    <>
-      <div className='slide-container'>
-        <Slide delayTime={4000}></Slide>
-      </div>
-    </>
-    
-  )
-}
+	const { DETAILS } = SLIDE_INFO;
 
-export default MainPage
+	function SlideList({ status, link, content }) {
+		return (
+			<li className='main-page__slide-item'>
+				<SlideCard status={status} />
+				<div className='main-page__slide-item-button-container'>
+					<Link to={link}>
+						<Button buttonSize={'button-slide'} content={content} />
+					</Link>
+				</div>
+			</li>
+		);
+	}
+
+	return (
+		<div className='main-page'>
+			<ul className='main-page__slide-list'>
+				{DETAILS.map((e, i) => {
+					return (
+						<SlideList
+							key={i}
+							status={e.status}
+							link={e.link}
+							content={e.content}
+						/>
+					);
+				})}
+			</ul>
+		</div>
+	);
+};
+
+export default MainPage;
