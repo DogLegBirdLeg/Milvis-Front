3a412c3ca3529be81099a32227e8765b8f6f5a11
Suyeon <msjgr2801@gmail.com>
2023-10-16 11:57:06 +0900

    Fix: API 명세 변경에 따른 오류 수정

--- src/components/Transfer/ResultTimeTable.js
+++ src/components/Transfer/ResultTimeTable.js
@@ -6,133 +6,133 @@ import ResultHeader from './ResultHeader';
 const BUS = 'bus';
 
 function ResultTimeTable({ type, timeSchedule }) {
-  const standardTimes = makeStandardTimes(type, timeSchedule); // 출발 시간 이후 한 시간 단위
-  const cardClassNames = makeCardClassNames(type, timeSchedule);
+	const standardTimes = makeStandardTimes(type, timeSchedule); // 출발 시간 이후 한 시간 단위
+	const cardClassNames = makeCardClassNames(type, timeSchedule);
 
-  const TimeSection = () => {
-    return (
-      <div class='time-table'>
-        {standardTimes.map((noticeTime, key) => {
-          return (
-            <div className='container-time-section' key={key}>
-              <NoticeTime noticeTime={noticeTime} />
-              <Cards noticeTime={noticeTime} />
-            </div>
-          );
-        })}
-        <div id='line'></div>
-      </div>
-    );
-  };
+	const TimeSection = () => {
+		return (
+			<div class='time-table'>
+				{standardTimes.map((noticeTime, key) => {
+					return (
+						<div className='container-time-section' key={key}>
+							<NoticeTime noticeTime={noticeTime} />
+							<Cards noticeTime={noticeTime} />
+						</div>
+					);
+				})}
+				<div id='line'></div>
+			</div>
+		);
+	};
 
-  const NoticeTime = ({ noticeTime }) => {
-    return (
-      <div className='circle-time' id={`${noticeTime}time`}>
-        {Number(noticeTime)}
-      </div>
-    );
-  };
+	const NoticeTime = ({ noticeTime }) => {
+		return (
+			<div className='circle-time' id={`${noticeTime}time`}>
+				{Number(noticeTime)}
+			</div>
+		);
+	};
 
-  const Cards = ({ noticeTime }) => {
-    if (type === TRAIN_OPTION.DEPART_ENG) {
-      return <DepartFromMilyangCards noticeTime={noticeTime} />;
-    }
+	const Cards = ({ noticeTime }) => {
+		if (type === TRAIN_OPTION.DEPART_ENG) {
+			return <DepartFromMilyangCards noticeTime={noticeTime} />;
+		}
 
-    if (type === TRAIN_OPTION.ARRIVE_ENG) {
-      return <ArriveToMilyangCards noticeTime={noticeTime} />;
-    }
-  };
+		if (type === TRAIN_OPTION.ARRIVE_ENG) {
+			return <ArriveToMilyangCards noticeTime={noticeTime} />;
+		}
+	};
 
-  const DepartFromMilyangCards = ({ noticeTime }) => {
-    return timeSchedule.map((schedule, index) => {
-      let time = undefined;
+	const DepartFromMilyangCards = ({ noticeTime }) => {
+		return timeSchedule.map((schedule, index) => {
+			let time = undefined;
 
-      if (schedule.type === BUS) {
-        time = Number(schedule.arriveTime.split(':')[0]);
-      } else {
-        time = Number(schedule.departTime.split(':')[0]);
-      }
-      if (time !== noticeTime) {
-        return;
-      }
+			if (schedule.type === BUS) {
+				time = Number(schedule.departTime.split(':')[0]);
+			} else {
+				time = Number(schedule.departTime.split(':')[0]);
+			}
+			if (time !== noticeTime) {
+				return;
+			}
 
-      return (
-        <Card
-          key={index}
-          cardClassName={cardClassNames[index]}
-          type={schedule.type === BUS ? 'depart-card' : 'arrive-card'}
-          name={schedule.name}
-          departTime={schedule.departTime}
-          arriveTime={schedule.arriveTime}
-        />
-      );
-    });
-  };
+			return (
+				<Card
+					key={index}
+					cardClassName={cardClassNames[index]}
+					type={schedule.type === BUS ? 'depart-card' : 'arrive-card'}
+					name={schedule.name}
+					departTime={schedule.departTime}
+					arriveTime={schedule.arriveTime}
+				/>
+			);
+		});
+	};
 
-  const ArriveToMilyangCards = ({ noticeTime }) => {
-    return timeSchedule.map((schedule, index) => {
-      let time = undefined;
+	const ArriveToMilyangCards = ({ noticeTime }) => {
+		return timeSchedule.map((schedule, index) => {
+			let time = undefined;
 
-      if (schedule.type === BUS) {
-        time = Number(schedule.departTime.split(':')[0]);
-      } else {
-        time = Number(schedule.arriveTime.split(':')[0]);
-      }
-      if (time !== noticeTime) {
-        return;
-      }
+			if (schedule.type === BUS) {
+				time = Number(schedule.departTime.split(':')[0]);
+			} else {
+				time = Number(schedule.arriveTime.split(':')[0]);
+			}
+			if (time !== noticeTime) {
+				return;
+			}
 
-      return (
-        <Card
-          key={index}
-          cardClassName={cardClassNames[index]}
-          type={schedule.type === BUS ? 'arrive-card' : 'depart-card'}
-          name={schedule.name}
-          departTime={schedule.departTime}
-          arriveTime={schedule.arriveTime}
-        />
-      );
-    });
-  };
+			return (
+				<Card
+					key={index}
+					cardClassName={cardClassNames[index]}
+					type={schedule.type === BUS ? 'arrive-card' : 'depart-card'}
+					name={schedule.name}
+					departTime={schedule.departTime}
+					arriveTime={schedule.arriveTime}
+				/>
+			);
+		});
+	};
 
-  const Card = ({ cardClassName, type, name, departTime, arriveTime }) => {
-    return (
-      <div className={`container-card ${cardClassName}`}>
-        <div className='depart-section'>
-          {type === 'depart-card' ? (
-            <div className='card'>
-              <div className='card-name'>{name}</div>
-              <div className='card-time'>
-                {departTime} - <span className='point'>{arriveTime}</span>
-              </div>
-            </div>
-          ) : (
-            ''
-          )}
-        </div>
-        <div className='point-circle'></div>
-        <div className='arrive-section'>
-          {type === 'arrive-card' ? (
-            <div className='card'>
-              <div className='card-name'>{name}</div>
-              <div className='card-time'>
-                <span className='point'>{departTime}</span> - {arriveTime}
-              </div>
-            </div>
-          ) : (
-            ''
-          )}
-        </div>
-      </div>
-    );
-  };
+	const Card = ({ cardClassName, type, name, departTime, arriveTime }) => {
+		return (
+			<div className={`container-card ${cardClassName}`}>
+				<div className='depart-section'>
+					{type === 'depart-card' ? (
+						<div className='card'>
+							<div className='card-name'>{name}</div>
+							<div className='card-time'>
+								{departTime} - <span className='point'>{arriveTime}</span>
+							</div>
+						</div>
+					) : (
+						''
+					)}
+				</div>
+				<div className='point-circle'></div>
+				<div className='arrive-section'>
+					{type === 'arrive-card' ? (
+						<div className='card'>
+							<div className='card-name'>{name}</div>
+							<div className='card-time'>
+								<span className='point'>{departTime}</span> - {arriveTime}
+							</div>
+						</div>
+					) : (
+						''
+					)}
+				</div>
+			</div>
+		);
+	};
 
-  return (
-    <div className='container-time-table'>
-      <ResultHeader type={type} />
-      <TimeSection />
-    </div>
-  );
+	return (
+		<div className='container-time-table'>
+			<ResultHeader type={type} />
+			<TimeSection />
+		</div>
+	);
 }
 
 export default ResultTimeTable;
--- src/utils/train/trainResult/makeCardMargin.js
+++ src/utils/train/trainResult/makeCardMargin.js
@@ -1,105 +1,98 @@
-import { TRAIN_OPTION } from "utils/Constant";
+import { TRAIN_OPTION } from 'utils/Constant';
 
-const BUS = "bus";
-const TRAIN = "train";
-
-const getTimeArriveBus = (schedule) => {
-  const time = schedule.arriveTime.split(":")[0];
-  const min = schedule.arriveTime.split(":")[1];
-
-  return [time, min];
-};
+const BUS = 'bus';
+const TRAIN = 'train';
 
 const getTimeDepartTrain = (schedule) => {
-  const time = schedule.departTime.split(":")[0];
-  const min = schedule.departTime.split(":")[1];
+	const time = schedule.departTime.split(':')[0];
+	const min = schedule.departTime.split(':')[1];
 
-  return [time, min];
+	return [time, min];
 };
 
 const getTimeDepartBus = (schedule) => {
-  const time = schedule.departTime.split(":")[0];
-  const min = schedule.departTime.split(":")[1];
+	const time = schedule.departTime.split(':')[0];
+	const min = schedule.departTime.split(':')[1];
 
-  return [time, min];
+	return [time, min];
 };
 
 const getTimeArriveTrain = (schedule) => {
-  const time = schedule.arriveTime.split(":")[0];
-  const min = schedule.arriveTime.split(":")[1];
+	const time = schedule.arriveTime.split(':')[0];
+	const min = schedule.arriveTime.split(':')[1];
 
-  return [time, min];
+	return [time, min];
 };
 
 const decideClassName = (time, classNames) => {
-  if (time < 5) {
-    classNames.push("none-margin");
-  } else if (time < 10) {
-    classNames.push("margin-1");
-  } else if (time < 20) {
-    classNames.push("margin-2");
-  } else if (time < 30) {
-    classNames.push("margin-3");
-  } else if (time < 40) {
-    classNames.push("margin-4");
-  } else {
-    classNames.push("margin-5");
-  }
+	if (time < 5) {
+		classNames.push('none-margin');
+	} else if (time < 10) {
+		classNames.push('margin-1');
+	} else if (time < 20) {
+		classNames.push('margin-2');
+	} else if (time < 30) {
+		classNames.push('margin-3');
+	} else if (time < 40) {
+		classNames.push('margin-4');
+	} else {
+		classNames.push('margin-5');
+	}
 };
 
 const departFromMilyang = (timeSchedule, classNames) => {
-  for (let idx = 0; idx < timeSchedule.length - 1; idx++) {
-    const nextSchedule = timeSchedule[idx + 1];
-    const [currTime, currMin] =
-      timeSchedule[idx].type === BUS
-        ? getTimeArriveBus(timeSchedule[idx])
-        : getTimeDepartTrain(timeSchedule[idx]);
-    const [nextTime, nextMin] =
-      nextSchedule.type === BUS
-        ? getTimeArriveBus(nextSchedule)
-        : getTimeDepartTrain(nextSchedule);
-    const diffTime = Number(nextTime) - Number(currTime);
-    const diffMin = Number(nextMin) - Number(currMin);
+	for (let idx = 0; idx < timeSchedule.length - 1; idx++) {
+		const nextSchedule = timeSchedule[idx + 1];
+		const [currTime, currMin] =
+			timeSchedule[idx].type === BUS
+				? getTimeDepartBus(timeSchedule[idx])
+				: getTimeDepartTrain(timeSchedule[idx]);
+		const [nextTime, nextMin] =
+			nextSchedule.type === BUS
+				? getTimeDepartBus(nextSchedule)
+				: getTimeDepartTrain(nextSchedule);
+		const diffTime = Number(nextTime) - Number(currTime);
+		const diffMin = Number(nextMin) - Number(currMin);
 
-    if (diffTime >= 1) {
-      classNames.push('none-margin');
-      continue;
-    }
+		if (diffTime >= 1) {
+			classNames.push('none-margin');
+			continue;
+		}
 
-    decideClassName(diffMin, classNames);
-  }
+		decideClassName(diffMin, classNames);
+	}
 };
 
 const arriveToMilyang = (timeSchedule, classNames) => {
-  for (let idx = 0; idx < timeSchedule.length - 1; idx++) {
-    const nextSchedule = timeSchedule[idx + 1];
-    const [currTime, currMin] =
-      timeSchedule[idx].type === BUS
-        ? getTimeDepartBus(timeSchedule[idx])
-        : getTimeArriveTrain(timeSchedule[idx]);
-    const [nextTime, nextMin] =
-      nextSchedule.type === BUS
-        ? getTimeDepartBus(nextSchedule)
-        : getTimeArriveTrain(nextSchedule);
-    const diffTime = Number(nextTime) - Number(currTime);
-    const diffMin = Number(nextMin) - Number(currMin);
+	for (let idx = 0; idx < timeSchedule.length - 1; idx++) {
+		const nextSchedule = timeSchedule[idx + 1];
+		const [currTime, currMin] =
+			timeSchedule[idx].type === BUS
+				? getTimeDepartBus(timeSchedule[idx])
+				: getTimeArriveTrain(timeSchedule[idx]);
+		const [nextTime, nextMin] =
+			nextSchedule.type === BUS
+				? getTimeDepartBus(nextSchedule)
+				: getTimeArriveTrain(nextSchedule);
+		const diffTime = Number(nextTime) - Number(currTime);
+		const diffMin = Number(nextMin) - Number(currMin);
 
-    if (diffTime >= 1) {
-      classNames.push('none-margin');
-      continue;
-    }
+		if (diffTime >= 1) {
+			classNames.push('none-margin');
+			continue;
+		}
 
-    decideClassName(diffMin, classNames);
-  }
+		decideClassName(diffMin, classNames);
+	}
 };
 
 export const makeCardClassNames = (type, timeSchedule) => {
-  const classNames = [];
-  if (type === TRAIN_OPTION.DEPART_ENG) {
-    departFromMilyang(timeSchedule, classNames);
-  } else {
-    arriveToMilyang(timeSchedule, classNames);
-  }
+	const classNames = [];
+	if (type === TRAIN_OPTION.DEPART_ENG) {
+		departFromMilyang(timeSchedule, classNames);
+	} else {
+		arriveToMilyang(timeSchedule, classNames);
+	}
 
-  return classNames;
+	return classNames;
 };
