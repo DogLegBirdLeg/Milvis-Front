25828fb34b4c2d2f561c2988a72d9a49c42a9e9b
Suyeon <msjgr2801@gmail.com>
2023-10-19 13:30:10 +0900

    Revert "Fix: API 명세 변경에 따른 오류 수정"
    This reverts commit 3a412c3ca3529be81099a32227e8765b8f6f5a11.

--- src/pages/TransferResultPage.js
+++ src/pages/TransferResultPage.js
@@ -6,26 +6,24 @@ import ResultInfo from 'components/Transfer/ResultInfo';
 import ResultTimeTable from 'components/Transfer/ResultTimeTable';
 
 function TransferResultPage() {
-	const { state } = useLocation();
-	const data = state;
+  const { state } = useLocation();
+  const data = state;
 
-	console.log(data);
-
-	return (
-		<div className='container-time-table-page'>
-			<ResultInfo
-				departStation={data.departStation}
-				arriveStation={data.arriveStation}
-				date={data.date}
-				time={data.time}
-			/>
-			<ResultTimeTable
-				matchTime={data.time}
-				type={data.type}
-				timeSchedule={data.sortSchedules}
-			/>
-		</div>
-	);
+  return (
+    <div className='container-time-table-page'>
+      <ResultInfo
+        departStation={data.departStation}
+        arriveStation={data.arriveStation}
+        date={data.date}
+        time={data.time}
+      />
+      <ResultTimeTable
+        matchTime={data.time}
+        type={data.type}
+        timeSchedule={data.sortSchedules}
+      />
+    </div>
+  );
 }
 
 export default TransferResultPage;
