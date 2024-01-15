c7b3232b5311061d2ce4d8a80d5c0efca10188e2
Suyeon <msjgr2801@gmail.com>
2024-01-15 16:26:11 +0900

    feat: 유효하지 않은 경로 막기

--- src/pages/TrainTransferResult/hooks/usePageLoad.js
+++ src/pages/TrainTransferResult/hooks/usePageLoad.js
@@ -15,11 +15,14 @@ function usePageLoad() {
 	const [loading, setLoading] = useState(true);
 	const [error, setError] = useState(false);
 
-	if (state === null) {
-		alert('올바른 경로로 접근해주세요.');
-		navigate('/train');
-	}
-	const { submitData } = state;
+	console.log(state);
+	console.log(error);
+
+	useEffect(() => {
+		if (state === null) {
+			setError(true);
+		}
+	}, [state, navigate]);
 
 	const submitForm = async ({
 		departDate,
@@ -54,21 +57,24 @@ function usePageLoad() {
 
 	const handleLoadWindow = useCallback(async () => {
 		try {
-			const data = await submitForm(submitData);
+			const data = await submitForm(state.submitData);
 
 			setData(data);
 		} catch (error) {
 			console.log(error);
-			// setError(true);
+			setError(true);
 		}
-	}, []);
+	}, [state]);
 
 	useEffect(() => {
-		if (error) {
+		if (error && state === null) {
+			alert('올바른 경로로 다시 접근해주세요!');
+			navigate('/train');
+		} else if (error) {
 			alert('서버에서 데이터를 받아오지 못했습니다. 다시 시도해주세요.');
 			navigate('/train');
 		}
-	}, [error]);
+	}, [error, navigate, state]);
 
 	useEffect(() => {
 		handleLoadWindow();
