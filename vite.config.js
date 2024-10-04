636e184f97aeeb77a6597e546b9e8a3ee2d81a56
Suyeon <msjgr2801@gmail.com>
2024-10-04 12:59:20 +0900

    refactoring: Vite JS 로 마이그레이션 하기

---
+++ vite.config.js
@@ -0,0 +1,11 @@
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+import svgrPlugin from 'vite-plugin-svgr';
+
+export default defineConfig({
+  plugins: [react(), svgrPlugin()],
+  server: {
+    port: 3000,
+  },
+  resolve: {},
+});
